fb0e4673ee9813213217b7b717fcd71741aa034a
Connor O'Hara <connor@switchboard.xyz>
2023-11-27 11:42:27 -0500

    add check

--- block/manager.go
+++ block/manager.go
@@ -501,6 +501,9 @@ func (m *Manager) processNextDABlock(ctx context.Context) error {
 			}
 			m.logger.Debug("retrieved potential blocks", "n", len(blockResp.Blocks), "daHeight", daHeight)
 			for _, block := range blockResp.Blocks {
+				if bytes.Equal(block.SignedHeader.ProposerAddress, m.genesis.Validators[0].Address) {
+					continue
+				}
 				blockHash := block.Hash().String()
 				m.blockCache.setDAIncluded(blockHash)
 				m.logger.Info("block marked as DA included", "blockHeight", block.Height(), "blockHash", blockHash)
